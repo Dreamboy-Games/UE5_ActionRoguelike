75297ee0fac3dfb68f9e7d365d2d43b8d2f75aee
Trent <waffledragon97@gmail.com>
2026-05-04 17:45:56 +1000

    Add BP_PlayerCharacter, Input bindings, and Course Assets

---
+++ Source/ActionRoguelike/Player/RogueCharacter.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+ARogueCharacter::ARogueCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->bUsePawnControlRotation = true;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+
+void ARogueCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ARogueCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+void ARogueCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	EnhancedInput->BindAction(Input_Move, ETriggerEvent::Triggered, this, &ARogueCharacter::Move);
+	EnhancedInput->BindAction(Input_Look, ETriggerEvent::Triggered, this, &ARogueCharacter::Look);
+}
+
+
+void ARogueCharacter::Move(const FInputActionValue& InValue) 
+{
+	const FVector2D InputValue = InValue.Get<FVector2D>();
+	
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	AddMovementInput(ControlRot.Vector(), InputValue.X);
+	
+	//const FVector RightDirection = GetActorRightVector();
+	const FVector RightDirection = ControlRot.RotateVector(FVector::RightVector);
+	AddMovementInput(RightDirection, InputValue.Y);
+}
+
+void ARogueCharacter::Look(const FInputActionInstance& InValue)
+{
+	const FVector2D InputValue = InValue.GetValue().Get<FVector2D>();
+	AddControllerPitchInput(InputValue.Y);
+	AddControllerYawInput(InputValue.X);
+}
---
+++ Source/ActionRoguelike/Player/RogueCharacter.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RogueCharacter.generated.h"
+
+struct FInputActionInstance;
+struct FInputActionValue;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueCharacter : public ACharacter {
+	GENERATED_BODY()
+
+public:
+	ARogueCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> Input_Move;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> Input_Look;
+	
+	void Move(const FInputActionValue& InValue);
+	void Look(const FInputActionInstance& InValue);
+};
