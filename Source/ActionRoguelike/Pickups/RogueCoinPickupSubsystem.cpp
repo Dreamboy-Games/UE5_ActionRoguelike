6eb0d36dc869030f64383f17238fdd264c59a0c5
Trent <waffledragon97@gmail.com>
2026-06-03 12:42:22 +1000

    DATA-ORIENTED DESIGN: added Coin Pickups, Iterating thousands of coins, coin Instanced Meshes, and Coin Pickup

---
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueCoinPickupSubsystem.h"
+
+#include "ActionRoguelike.h"
+#include "EngineUtils.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Player/RoguePlayerCharacter.h"
+
+void URogueCoinPickupSubsystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+	
+	UWorld* World = GetWorld();
+	
+	// Temporary Hack
+	const FSoftObjectPath MeshAssetPath(TEXT("/Game/ExampleContent/Meshes/SM_Pickup_Coin.SM_Pickup_Coin"));
+	UStaticMesh* LoadedMesh = Cast<UStaticMesh>(MeshAssetPath.TryLoad());
+	
+	WorldISM = NewObject<UInstancedStaticMeshComponent>(World, NAME_None, RF_Transient);
+	WorldISM->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	WorldISM->SetStaticMesh(LoadedMesh);
+	WorldISM->RegisterComponentWithWorld(World);
+}
+
+void URogueCoinPickupSubsystem::AddCoinPickups(TArray<FVector> NewLocations, TArray<int32> NewAmounts)
+{
+	CoinLocations.Append(NewLocations);
+	CoinAmounts.Append(NewAmounts);
+	
+	TArray<FTransform> MeshTransforms;
+	for (int i = 0; i < NewLocations.Num(); i++)
+	{
+		MeshTransforms.Add(FTransform(NewLocations[i] + FVector(0, 0, 50.0f)));
+	}
+
+	const TArray<FPrimitiveInstanceId> NewMeshIDs = WorldISM->AddInstancesById(MeshTransforms, true, false);
+	MeshIDs.Append(NewMeshIDs);
+}
+
+void URogueCoinPickupSubsystem::RemoveCoinPickup(const int32 IndexToRemove)
+{
+	CoinLocations.RemoveAt(IndexToRemove);
+	CoinAmounts.RemoveAt(IndexToRemove);
+	
+	WorldISM->RemoveInstanceById(MeshIDs[IndexToRemove]);
+	MeshIDs.RemoveAt(IndexToRemove);
+}
+
+void URogueCoinPickupSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	const UWorld* World = GetWorld();
+	
+	FVector PlayerLocation = FVector::ZeroVector;
+	for (const ARoguePlayerCharacter* PlayerCharacter : TActorRange<ARoguePlayerCharacter>(World))
+	{
+		PlayerLocation = PlayerCharacter->GetActorLocation();
+	}
+	
+	const float PickupRadius = 200.0f;
+	
+	TArray<int32> ProcessList;
+	
+	for (int i = 0; i < CoinLocations.Num(); i++)
+	{
+		const float Dist = FVector::Dist(PlayerLocation, CoinLocations[i]);
+		if (Dist < PickupRadius)
+		{
+			ProcessList.Add(i);
+		}
+	}
+	
+	int32 TotalCoinsToGrant = 0;
+	for (int i = ProcessList.Num()-1; i >= 0; --i)
+	{
+		const int32 CoinIndex = ProcessList[i];
+		TotalCoinsToGrant += CoinAmounts[CoinIndex];
+		RemoveCoinPickup(CoinIndex);
+	}
+	
+	// @todo: grant coins to player(s)
+	UE_CLOG(TotalCoinsToGrant > 0, LogGame, Log, TEXT("Pickup Coin Amount = %d"), TotalCoinsToGrant);
+	
+	for (int i = 0; i < CoinLocations.Num(); i++)
+	{
+		DrawDebugPoint(World, CoinLocations[i], 8.0f, FColor::White);
+	}
+}
---
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RogueCoinPickupSubsystem.generated.h"
+
+class UInstancedStaticMeshComponent;
+struct FPrimitiveInstanceId;
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API URogueCoinPickupSubsystem : public UTickableWorldSubsystem {
+	GENERATED_BODY()
+	// UTickableWorldSubsystem lifetime tied to level
+public:
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	
+	void AddCoinPickups(TArray<FVector> NewLocations, TArray<int32> CoinAmounts);
+	
+	void RemoveCoinPickup(int32 IndexToRemove);
+	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual TStatId GetStatId() const override
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(RogueCoinPickupSubsystem, STATGROUP_Tickables);
+	}
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<UInstancedStaticMeshComponent> WorldISM;
+	
+	TArray<FVector> CoinLocations;
+	TArray<int32> CoinAmounts;
+	TArray<FPrimitiveInstanceId> MeshIDs;
+};
